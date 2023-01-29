d3746c377e780b685586c505d32d2770fbb2ed7e
florianfischer91 <72799603+florianfischer91@users.noreply.github.com>
2023-01-29 10:52:05 +0100

    Create custom_step.groovy

---
+++ vars/custom_step.groovy
@@ -0,0 +1,19 @@
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    container(name: 'test') {
+        for (int i = 0; i < config.messages.size(); i++) {
+            //image = config.images[i]
+            //retry(3) {
+            //    sh "docker pull ${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/fabric8/${image}:${config.tag}"
+            //    sh "docker tag  ${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/fabric8/${image}:${config.tag} docker.io/fabric8/${image}:${config.tag}"
+            //    sh "docker push docker.io/fabric8/${image}:${config.tag}"
+            //}
+            sh "Sending a message: ${config.messages[i]}"
+        }
+    }
+}
