38e6c4154cf95f10c14e87f8d90d0d77085007c1
florianfischer91 <florian91.fischer@gmail.com>
2023-09-09 17:32:44 +0200

    adjust custom_step

--- vars/custom_step.groovy
+++ vars/custom_step.groovy
@@ -1,19 +1,19 @@
-def call(body) {
+def call(Map params= [:]) {
     // evaluate the body block, and collect configuration into the object
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
+    // def config = [:]
+    // body.resolveStrategy = Closure.DELEGATE_FIRST
+    // body.delegate = config
+    // body()
 
     //container(name: 'test') {
-        for (int i = 0; i < config.messages.size(); i++) {
+        for (int i = 0; i < params.messages.size(); i++) {
             //image = config.images[i]
             //retry(3) {
             //    sh "docker pull ${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/fabric8/${image}:${config.tag}"
             //    sh "docker tag  ${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/fabric8/${image}:${config.tag} docker.io/fabric8/${image}:${config.tag}"
             //    sh "docker push docker.io/fabric8/${image}:${config.tag}"
             //}
-            sh 'echo "Sending a message: ${config.messages[i]}"'
+            sh "echo 'Sending a message: ${params.messages[i]}'"
         }
     //}
 }
